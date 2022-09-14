e7b0b1960800e504e7d97d6fd9c66538d7db4282
Vinicius Lopes Alves <vilopesalves@hotmail.com>
2022-09-14 02:53:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// /*==================== LOADING ANIMATION ====================*/
+// function load (){
+//     document.getElementById("loading").style.display = "none";
+//     document.getElementById("conteudo").style.display = "inherit";
+// }
+
+// window.onload = (function () { window.setInterval('load()',2000);})
